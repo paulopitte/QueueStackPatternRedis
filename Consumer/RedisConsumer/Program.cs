9f2a915a5d14828eaf01a6f77230f0c40909f8d5
Paulo Pitte <pauloanalista@outlook.com>
2025-03-26 12:42:49 -0300

    implementação basica

---
+++ Consumer/RedisConsumer/Program.cs
@@ -0,0 +1,22 @@
+﻿using StackExchange.Redis;
+
+namespace RedisConsumer;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var conn = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+        var database = conn.GetDatabase();
+
+        var key = "369";
+        Console.ForegroundColor = ConsoleColor.Yellow;
+
+        while (true)
+        {
+            var data = database.ListLeftPop(key);
+            Console.WriteLine($"{DateTime.UtcNow:o} => {data}");
+            Thread.Sleep(100);
+        }
+
+    }
+}
---
+++ Producer/RedisProducer/Program.cs
@@ -0,0 +1,22 @@
+﻿using StackExchange.Redis;
+namespace RedisProducer;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var conn = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+        var database = conn.GetDatabase();
+
+        var key = "369";
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        for (var index = 1; index < 100; index++)
+        {
+            database.ListLeftPush($"{key}", " order: " + index + ", value: 10000}");
+            Console.WriteLine($"{DateTime.UtcNow:o} ==> published {index}");
+        }
+
+        Console.WriteLine($"{DateTime.UtcNow:o} ===========> end");
+    }
+}
